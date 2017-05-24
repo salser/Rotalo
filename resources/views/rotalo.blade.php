8b0b6b2f0de58dee10e58c5221a324a1cd47d047
Henry Salazar <henrysteven003@gmail.com>
2017-05-23 23:48:38 -0500

    bugs fixed ND TEXT CHANGED IN WHO WE ARE Henry Salazar

--- resources/views/rotalo.blade.php
+++ resources/views/rotalo.blade.php
@@ -9,55 +9,24 @@
       <ul class="collapsible popout" data-collapsible="accordion">
         <li>
           <div class="collapsible-header active"><i class="material-icons">filter_drama</i>¿Quíenes Somos?</div>
-          <div class="collapsible-body"><span>Ne minim malis an officia do hic enim minim de fabulas, ipsum aut arbitror ab
-            tamen, quamquam aut quae vidisse ab o irure quamquam, o dolor amet duis
-            incurreret, appellat esse illum aliquip noster, litteris illum excepteur. Tempor
-            relinqueret cupidatat noster singulis, nam dolore eruditionem. Quae voluptatibus
-            excepteur quis quibusdam ex quo amet constias ab deserunt philosophari te
-            quamquam est illum te fabulas ad quem. Si duis expetendis expetendis, an ea sint
-            appellat.Te singulis est possumus. Litteris veniam magna litteris quid, anim
-            arbitror praetermissum. Incurreret transferrem ex iudicem, ita ex anim dolor
-            illum, constias velit cillum nescius minim, summis ex in quorum mentitum. De
-            duis arbitror possumus a ad ut quorum fore magna nam fugiat probant offendit, ut
-            cillum sint magna eiusmod ex fugiat cernantur philosophari, magna cupidatat non
-            multos enim de ut proident comprehenderit sed ut magna magna labore iudicem.</span></div>
+          <div class="collapsible-body"><span>Rótalo es una plataforma web gestora de trueques. En nuestro “mercado” creemos que la basura
+             de uno es el tesoro de otro. Por ello promovemos a que las personas intercambien aquellos objetos que ya no utilizan y por los que
+              pueden obtener un beneficio sin necesidad de dinero. Además esperamos que  al intercambiar los bienes o productos desechados y darles un uso
+               igual o diferente a aquel para el que fueron concebidos ayudemos firmemente al medio ambiente.</span></div>
         </li>
         <li>
           <div class="collapsible-header"><i class="material-icons">place</i>Misión</div>
-          <div class="collapsible-body"><span>Est esse cupidatat excepteur, iis aliquip tractavissent si cupidatat sunt fore
-            eiusmod quis, ubi culpa vidisse cohaerescant. Expetendis amet sunt in duis non
-            nisi do de magna nescius, se ea multos expetendis, eu elit tamen quae
-            incurreret. Deserunt culpa cillum a tempor. Cupidatat e elit eiusmod an culpa se
-            e multos excepteur ne proident legam ita vidisse concursionibus, fugiat
-            voluptate graviterque, eiusmod quo fore sed ab quis commodo laboris ne culpa
-            commodo quamquam iis fugiat do non eram vidisse.Anim probant despicationes,
-            proident veniam tempor o dolore. Summis appellat aut minim tamen ita probant
-            nulla eiusmod iis consequat sempiternum iis singulis ex a duis exercitation. Ex
-            ab imitarentur non e constias reprehenderit, dolor mandaremus efflorescere, sunt
-            quibusdam proident, elit aliquip quo ipsum nulla, occaecat legam sint fabulas
-            export, fugiat se doctrina ea anim, quis praesentibus incurreret nulla eiusmod.
-            Illum occaecat probant, sint coniunctione offendit sunt possumus ita si id
-            illustriora de quae vidisse vidisse eu magna vidisse o esse eram se appellat
-            minim amet possumus quae hic in de sint illum enim ubi probant philosophari ut
-            consequat.</span></div>
+          <div class="collapsible-body"><span>Rótalo es una plataforma web gestora de trueques. En nuestro “mercado” creemos que la basura
+             de uno es el tesoro de otro. Por ello promovemos a que las personas intercambien aquellos objetos que ya no utilizan y por los que
+              pueden obtener un beneficio sin necesidad de dinero. Además esperamos que  al intercambiar los bienes o productos desechados y darles un uso
+               igual o diferente a aquel para el que fueron concebidos ayudemos firmemente al medio ambiente.</span></div>
         </li>
         <li>
           <div class="collapsible-header"><i class="material-icons">whatshot</i>Visión</div>
-          <div class="collapsible-body"><span>Ipsum ut nescius aut quorum incurreret instituendarum, litteris fugiat tamen
-            singulis fore, officia cillum velit si amet. De dolor fugiat sed singulis, si
-            ita malis quae ipsum se officia philosophari si fabulas, et sunt ubi quis. Esse
-            aliquip praesentibus. Est aliquip do probant, labore excepteur excepteur.Nam
-            sint possumus imitarentur ut ex eram laborum reprehenderit si magna doctrina quo
-            consectetur, cupidatat o summis, export excepteur iudicem in eu quae
-            reprehenderit, aliqua cupidatat singulis, incurreret summis in commodo
-            graviterque. Cillum praesentibus proident cillum cernantur, dolor vidisse ea
-            offendit, cernantur ab quae nescius ex tempor possumus ut coniunctione. Mentitum
-            iis labore ita aliquip quem tamen an sint non proident aute tempor est eram ab
-            probant ipsum quem occaecat dolor eu de iudicem te singulis, ne velit
-            cohaerescant ab quae laborum nescius, quo aut imitarentur. Nam elit incididunt
-            eu quid litteris ab coniunctione, minim arbitror incurreret, o ab quid
-            cernantur, nostrud ea anim pariatur, admodum non dolor ullamco de aute
-            efflorescere litteris noster commodo, mentitum hic incurreret.</span></div>
+          <div class="collapsible-body"><span>Rótalo es una plataforma web gestora de trueques. En nuestro “mercado” creemos que la basura
+             de uno es el tesoro de otro. Por ello promovemos a que las personas intercambien aquellos objetos que ya no utilizan y por los que
+              pueden obtener un beneficio sin necesidad de dinero. Además esperamos que  al intercambiar los bienes o productos desechados y darles un uso
+               igual o diferente a aquel para el que fueron concebidos ayudemos firmemente al medio ambiente.</span></div>
         </li>
       </ul>
     </div>
